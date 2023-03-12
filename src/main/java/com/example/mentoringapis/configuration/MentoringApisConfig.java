50b1c2af86dda6b72f0729e90b1cad2dfc869452
vulam270403@gmail.com <hitran.m1@gmail.com>
2023-03-12 14:12:22 +0700

    first

---
+++ src/main/java/com/example/mentoringapis/configuration/MentoringApisConfig.java
@@ -0,0 +1,8 @@
+package com.example.mentoringapis.configuration;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+//@SpringBootConfiguration
+public class MentoringApisConfig {
+
+}
---
+++ src/test/java/com/example/mentoringapis/MentoringApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentoringapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
